218d43af9653152dddc7c856fd2958b06d63f3b5
Darcie <darciewalsh@hotmail.com>
2018-08-24 12:33:38 +0100

    Added student names

---
+++ directory.rb
@@ -0,0 +1,17 @@
+student_count = 11
+# print the list of students
+puts "The students of Villains Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vadar"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+
+puts "Overall, we have #{student_count} great students"
